69117d1d94dc720c812f7a6bf6daccf3405a2166
DK\whc <whc@sitecore.net>
2018-11-16 18:55:30 +0800

    Fix SXA Search Box cannot search Unicode characters in IE 11 due to lack of URL encoding

---
+++ src/Sitecore.Support.293668/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.293668")]
+[assembly: AssemblyProduct("Sitecore.Support.293668")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.293668/component-search-ajax.js
@@ -0,0 +1,35 @@
+XA.component.search.ajax = (function ($, document) {
+    var ApiModel, getPrameterByName;
+
+    ApiModel = Backbone.Model.extend({
+        getData: function (properties) {
+            var siteName = this.getPrameterByName("sc_site"),
+                url = typeof properties.excludeSiteName !== "undefined" && properties.excludeSiteName
+                    ? properties.url
+                    : XA.component.search.url.createSiteUrl(properties.url, siteName);
+			var searchQuery = this.getPrameterByName("q");// Patch 293668 Start
+			if (searchQuery){
+				url = url.replace(searchQuery, encodeURIComponent(searchQuery));
+			}// Patch 293668 End
+            Backbone.ajax({
+                dataType: "json",
+                url: url,
+                success: function(data){
+                    properties.callback(data);
+                }
+            });
+        },
+        getPrameterByName: function (name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[#?&]" + name + "(=([^&#]*)|&|#|$)"),// Patch 293668 added # as in [#?&]
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        } 
+    });
+
+    return new ApiModel();
+
+}(jQuery, document));
